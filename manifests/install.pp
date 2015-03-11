dbb34e030ca0e2af05e0ac5c764e13bed7dac2da
Craig Carnell <craig_carnell@hotmail.com>
2015-03-11 12:41:03 +0000

    fix shared compat libraries with 5.6 install

--- manifests/install.pp
+++ manifests/install.pp
@@ -47,6 +47,12 @@ class percona::install {
             default => $::percona::pkg_compat,
           }
         }
+        '5.6': {
+          $pkg_compat = $::percona::pkg_compat ? {
+            undef   => 'Percona-Server-shared-compat',
+            default => $::percona::pkg_compat,
+          }
+        }
         default: {
           $pkg_compat = $::percona::pkg_compat ? {
             undef   => 'Percona-SQL-shared-compat',
